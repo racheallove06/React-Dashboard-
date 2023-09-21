cc959f785bec42ecd7aef381e8aebf7ea2f37336
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-21 18:53:19 +0300

    created the first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
 
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1 className="underline text-3xl">Hello World</h1>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            {" "}
+            <TooltipComponent content="Setting" position="Top">
+              {" "}
+              <button>
+                <FiSettings />
+              </button>
+            </TooltipComponent>{" "}
+          </div>
+        </div>
+      </BrowserRouter>{" "}
     </div>
   );
 }
