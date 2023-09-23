604969ac3faa56cea4a8c5e598e82812e09f010a
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-23 13:21:13 +0300

    added some sparkline content

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,2 +1,32 @@
 import React from "react";
-export default function SparkLine() {}
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
+export default function SparkLine({
+  id,
+  height,
+  width,
+  color,
+  data,
+  type,
+  currentColor,
+}) {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      type={type}
+    >
+      <Inject services={SparklineTooltip} />
+    </SparklineComponent>
+  );
+}
