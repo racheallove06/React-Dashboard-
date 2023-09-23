df9e59024a09e712ac887fea0be3d176edd6ee75
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-23 22:54:30 +0300

    made the orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnsDirectives,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+import { ordersData, ContextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div>
+      <Header category="Page" title="Orders" />
+    </div>
+  );
 };
 
 export default Orders;
