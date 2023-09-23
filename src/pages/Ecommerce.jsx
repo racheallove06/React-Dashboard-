2229f2214315cd02587d46dc263b370384df1680
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-23 11:00:30 +0300

    modified e-commerce jsx

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -10,15 +10,19 @@ import {
 
 import { useStateContext } from "../contexts/ContextProvider";
 
-const Ecommerce = () => {
+export default function Ecommerce() {
   return (
-    <div className=" mt-12">
+    <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        {" "}
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center  "></div>{" "}
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center  ">
+            <div>
+              <p className="font-bold text-gray-400">Earnings</p>
+              <p className=" text-2xl">$6752</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-};
-
-export default Ecommerce;
+}
