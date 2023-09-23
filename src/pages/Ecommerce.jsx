48cc63ca6a05af3f3cf7ad15047ffef4927e3ff6
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-23 12:49:15 +0300

    added nother div section

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BsCurrencyDollar } from "react-icons/bs";
-import { GoPrimitiveDot } from "react-icons/go";
+import { GoDot } from "react-icons/go";
 import { Stacked, Pie, Button, SparkLine } from "../components";
 import {
   earningData,
@@ -31,36 +31,66 @@ export default function Ecommerce() {
             />
           </div>
         </div>
-      </div>
-      <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-        {" "}
-        {earningData.map((item) => (
-          <div
-            key={item.title}
-            className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
-          >
-            {" "}
-            <button
-              type="button"
-              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {" "}
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
             >
               {" "}
-              {item.icon}{" "}
-            </button>
-            <p className="mt-3">
-              {" "}
-              <span className="text-lg font-semibold">
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+              >
                 {" "}
-                {item.amount}{" "}
-              </span>{" "}
-              <span className={`text-sm text-${item.pcColor} ml-2`}>
+                {item.icon}{" "}
+              </button>
+              <p className="mt-3">
                 {" "}
-                {item.percentage}
-              </span>
-            </p>
+                <span className="text-lg font-semibold">
+                  {" "}
+                  {item.amount}{" "}
+                </span>{" "}
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {" "}
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+            </div>
+          ))}{" "}
+        </div>
+      </div>
+      {/*revenue section */}
+      <div className="flex gap-10 flex-wrap justify-center ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl ">
+                {" "}
+                <span>
+                  {" "}
+                  <GoDot />
+                </span>
+                <span>Expense </span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl ">
+                {" "}
+                <span>
+                  {" "}
+                  <GoDot />
+                </span>
+                <span>Budget </span>
+              </p>
+            </div>
           </div>
-        ))}{" "}
+          <div className="mt-10 flex gap-10 flex-wrap justify-center ">
+            <div className="border-r-1 border-color m-4 pr-10"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
