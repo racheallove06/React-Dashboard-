4f04e42f909780a9a0d0d57d32378d5d4fe1bee2
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-23 22:33:11 +0300

    modified the stacked

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -8,27 +8,33 @@ import {
   Category,
   StackingColumnSeries,
   Tooltip,
-} from "@syncfusion/ej2/office-chart";
+} from "@syncfusion/ej2-react-charts";
+
 import {
   stackedCustomSeries,
   stackedPrimaryXAxis,
   stackedPrimaryYAxis,
 } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Stacked = ({ width, height }) => {
+  const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
       width={width}
       height={height}
-      id="charts"
-      // primaryXAxis={stackedPrimaryXAxis}
-      //primaryYAxis={stackedPrimaryYAxis}
       chartArea={{ border: { width: 0 } }}
       tooltip={{ enable: true }}
-      LegendSettings={{ background: "white" }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      legendSettings={{ background: "white" }}
     >
-      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
       <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         {stackedCustomSeries.map((item, index) => (
           <SeriesDirective key={index} {...item} />
         ))}
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -115,19 +115,19 @@ export default function Ecommerce() {
                   data={SparklineAreaData}
                   color="blue"
                 />
+              </div>{" "}
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download report"
+                  borderRadius="10px"
+                />
               </div>
+            </div>{" "}
+            <div>
+              <Stacked width="320px" height="360px" />
             </div>
-            <div className="mt-10">
-              <Button
-                color="white"
-                bgColor="blue"
-                text="Download report"
-                borderRadius="10px"
-              />
-            </div>
-          </div>
-          <div>
-            <Stacked width="320px" height="360px" />
           </div>
         </div>
       </div>
