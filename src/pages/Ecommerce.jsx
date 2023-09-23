6cc57f87a752a7485114c1625955bfb00188211a
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-23 11:46:17 +0300

    added an icon

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -31,6 +31,24 @@ export default function Ecommerce() {
             />
           </div>
         </div>
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {" "}
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+            >
+              {" "}
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+              >
+                {" "}
+                {item.icon}{" "}
+              </button>
+            </div>
+          ))}{" "}
+        </div>
       </div>
     </div>
   );
