895d30bedeb1a6bf55f5a7013d4bb29c9da6528d
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-24 22:00:27 +0300

    added the inject component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -20,12 +20,24 @@ const Orders = () => {
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
       {/* where the data goes */}
-      <GridComponent id="gridcomp" dataSource={ordersData}>
+      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging>
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
       </GridComponent>
     </div>
   );
