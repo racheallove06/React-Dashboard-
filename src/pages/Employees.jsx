7b336d84168ed947c192184e5a1ae270cf94c95c
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-24 22:12:41 +0300

    added the employees page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,51 @@
 import React from "react";
-
-const Employees = () => {
-  return <div>Employees</div>;
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+import { ordersData, ContextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
+const Orders = () => {
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      {/* where the data goes */}
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
-export default Employees;
+export default Orders;
