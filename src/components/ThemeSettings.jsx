755ec7edeaa9e1a2992e1d5a292ff77aee5a3c59
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-25 21:41:33 +0300

    added the input

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -9,7 +9,7 @@ const ThemeSettings = () => {
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
       <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400 ">
         <div className="flex justify-between items-center p-4 ml-4">
-          <p className="font-semibold text-lg">Settings</p>
+          <p className="font-semibold text-xl">Settings</p>
           <button
             type="button"
             onclick={() => {}}
@@ -19,6 +19,21 @@ const ThemeSettings = () => {
             {" "}
             <MdOutlineCancel />
           </button>
+        </div>
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-lg">Theme Options</p>
+
+          <div className="mt-4">
+            <input
+              type="radio"
+              id="light"
+              name="theme"
+              value="Light"
+              className="cursor-pointer"
+              onChange={() => {}}
+              checked={true}
+            />
+          </div>
         </div>
       </div>
     </div>
