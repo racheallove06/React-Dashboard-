1f3890e64845df63138b2f1df35004071f39898e
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-25 22:18:56 +0300

    added the theme colors

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -55,6 +55,33 @@ const ThemeSettings = () => {
         </div>
         <div className="flex-col border-t-1 border-color p-4 ml-4">
           <p className="font-semibold text-lg">Theme Colors</p>
+          <div className=" flex gap-3">
+            {themeColors.map((item, index) => (
+              <TooltipComponent
+                key={index}
+                content={item.name}
+                position="TopCenter"
+              >
+                {" "}
+                <div className="relative mt-2 cursor-pointer flex gap-5 items-center ">
+                  {" "}
+                  <button
+                    type="button"
+                    className="h-10 w-10 rounded-full cursor-pointer "
+                    style={{ backgroundColor: item.color }}
+                    onClick={() => {}}
+                  >
+                    {" "}
+                    <BsCheck
+                      className={`ml-2 text-2xl text-white ${
+                        item.color === currentColor ? "block" : "hidden"
+                      }`}
+                    />
+                  </button>
+                </div>
+              </TooltipComponent>
+            ))}
+          </div>
         </div>
       </div>
     </div>
