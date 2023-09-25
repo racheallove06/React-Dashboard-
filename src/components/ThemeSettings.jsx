6d90d5c4c820c50a4445727338b7e4b5e49279a5
rachealmuchiri <rachealloveo6@gmail.com>
2023-09-25 19:28:07 +0300

    added the button

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -10,6 +10,15 @@ const ThemeSettings = () => {
       <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400 ">
         <div className="flex justify-between items-center p-4 ml-4">
           <p className="font-semibold text-lg">Settings</p>
+          <button
+            type="button"
+            onclick={() => {}}
+            style={{ color: "rgb(153 , 171, 180", borderRadius: "50%" }}
+            className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray "
+          >
+            {" "}
+            <MdOutlineCancel />
+          </button>
         </div>
       </div>
     </div>
