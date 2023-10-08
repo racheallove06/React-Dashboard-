37c2f40aa29036df378e583fc586f3b718def224
rachealmuchiri <rachealloveo6@gmail.com>
2023-10-08 22:07:41 +0300

    modified dummy.js

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -15,10 +15,10 @@ export default function Ecommerce() {
   return (
     <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+        <div className="bg-[#9d6ae8] dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3  bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center  ">
             <div>
-              <p className="font-bold text-gray-400">Earnings</p>
+              <p className="font-bold ">Earnings</p>
               <p className=" text-2xl">$6752</p>
             </div>
           </div>
@@ -32,45 +32,48 @@ export default function Ecommerce() {
             />
           </div>
         </div>
-        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-          {" "}
-          {earningData.map((item) => (
-            <div
-              key={item.title}
-              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+      </div>
+      <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+        {" "}
+        {earningData.map((item) => (
+          <div
+            key={item.title}
+            className="bg-[#d9f2ff] dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl shadow-lg shadow-gray-200"
+          >
+            {" "}
+            <button
+              type="button"
+              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+              className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
             >
               {" "}
-              <button
-                type="button"
-                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
-              >
+              {item.icon}{" "}
+            </button>
+            <p className="mt-3">
+              {" "}
+              <span className="text-lg font-semibold">
                 {" "}
-                {item.icon}{" "}
-              </button>
-              <p className="mt-3">
+                {item.amount}{" "}
+              </span>{" "}
+              <span className={`text-sm text-${item.pcColor} ml-2`}>
                 {" "}
-                <span className="text-lg font-semibold">
-                  {" "}
-                  {item.amount}{" "}
-                </span>{" "}
-                <span className={`text-sm text-${item.pcColor} ml-2`}>
-                  {" "}
-                  {item.percentage}
-                </span>
-              </p>
-              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
-            </div>
-          ))}{" "}
-        </div>
+                {item.percentage}
+              </span>
+            </p>
+            <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+          </div>
+        ))}{" "}
       </div>
+
       {/*revenue section */}
       <div className="flex gap-10 flex-wrap justify-center ">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+        <div className="bg-[#d9f2ff] dark:text-[#e4f1fe] dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780 shadow-md shadow-gray-200">
           <div className="flex justify-between">
-            <p className="font-semibold text-xl">Revenue Updates</p>
+            <p className="font-semibold text-xl text-[#f76b8a]">
+              Revenue Updates
+            </p>
             <div className="flex items-center gap-4">
-              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl ">
+              <p className="flex items-center gap-2 text-[#f76b8a] hover:drop-shadow-xl ">
                 {" "}
                 <span>
                   {" "}
@@ -78,7 +81,7 @@ export default function Ecommerce() {
                 </span>
                 <span>Expense </span>
               </p>
-              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl ">
+              <p className="flex items-center gap-2 text-[#f76b8a] hover:drop-shadow-xl ">
                 {" "}
                 <span>
                   {" "}
