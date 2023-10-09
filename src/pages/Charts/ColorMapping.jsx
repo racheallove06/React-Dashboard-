70a0855472ef8e66d5764c810990bfd724bdf6b3
rachealmuchiri <rachealloveo6@gmail.com>
2023-10-09 22:05:30 +0300

    modified dummy data

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -5,21 +5,53 @@ import {
   SeriesDirective,
   Inject,
   Legend,
-  DateTime,
   Tooltip,
-  DataLabel,
-  LineSeries,
+  RangeColorSettingsDirective,
+  RangeColorSettingDirective,
+  ColumnSeries,
 } from "@syncfusion/ej2-react-charts";
+import {
+  colorMappingData,
+  ColorMappingPrimaryXAxis,
+  ColorMappingPrimaryYAxis,
+  rangeColorMapping,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 import { Header } from "../../components";
+
 const ColorMapping = () => {
+  const { currentMode } = useStateContext();
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-[#d9f2ff] dark:bg-secondary-dark-bg rounded-3xl">
       <Header title="Color Mapping Data" category="Color Mapping" />
-      <ChartComponent>
-        <Inject services={[Legend, DataLabel, Tooltip, LineSeries]} />
+      <ChartComponent
+        id="charts"
+        primaryXAxis={ColorMappingPrimaryXAxis}
+        primaryYAxis={ColorMappingPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        legendSettings={{ mode: "Range", background: "white" }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      >
+        <Inject services={[Legend, Tooltip, ColumnSeries]} />
         <SeriesCollectionDirective>
-          <SeriesDirective />
+          <SeriesDirective
+            dataSource={colorMappingData[0]}
+            name="Kenya"
+            xName="x"
+            yName="y"
+            type="Column"
+            cornerRadius={{
+              topLeft: 10,
+              topRight: 10,
+            }}
+          />
         </SeriesCollectionDirective>
+        <RangeColorSettingsDirective>
+          {rangeColorMapping.map((item, index) => (
+            <RangeColorSettingDirective key={index} {...item} />
+          ))}
+        </RangeColorSettingsDirective>
       </ChartComponent>
     </div>
   );
