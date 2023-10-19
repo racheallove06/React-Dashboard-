92d10ea86ed9b2b754c3acdf8906f4f3e051cb74
Racheal Muchiri <rachealloveo6@gmail.com>
2023-10-19 21:48:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";        
 import "./App.css";
 
 import { Navbar, Footer, SideBar, ThemeSettings } from "./components";
